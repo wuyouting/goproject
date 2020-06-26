db8755cd741eec78c8a89b4ded20cda2af921b9b
wuyouting <286114755@qq.com>
2020-06-26 18:29:09 +0800

    testaa

--- test.go
+++ test.go
@@ -6,6 +6,10 @@ import (
 	"time"
 )
 
+// chan *item ：既能接收也能发送的一个队列
+// chan <- *item :只能发送的一个队列（单向通道）
+// <-chan *item :只能接收一个队列（单向通道）
+
 func getInput() {
 	tmp := [1]byte{}
 	os.Stdin.Read(tmp[:])

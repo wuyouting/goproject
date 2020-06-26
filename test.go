ecc819f893f96a50d7c1bba2ace90497b392bf3f
wuyouting <286114755@qq.com>
2020-06-26 18:01:46 +0800

    test

---
+++ test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func getInput() {
+	tmp := [1]byte{}
+	os.Stdin.Read(tmp[:])
+	fmt.Println(123)
+
+}
+func main() {
+	getInput()
+	time.Sleep(100 * time.Second)
+}
